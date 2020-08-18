8dbdb18a5e4cb1d6a974273fc96797245ea8df4b
Kevin Elliott <kevin@welikeinc.com>
2020-08-18 15:30:39 -0700

    Create install-from-web.sh

---
+++ install-from-web.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "[BoxOS] Updating apt and installing git..."
+apt update && apt install -y git
+
+echo "[BoxOS] Fetching install scripts from https://github.com/boxelio/boxel-scripts"
+git clone https://github.com/boxelio/boxel-scripts /tmp/boxel-scripts
+cd /tmp/boxel-scripts
+chmod +x install.sh && ./install.sh
