746643c96ef5bb96bc2c58843bb1d19efba813b9
Kevin Elliott <kevin@welikeinc.com>
2021-01-20 20:16:49 -0800

    Misc

--- install-base-from-web.sh
+++ install-base-from-web.sh
@@ -4,6 +4,7 @@ echo "[BoxOS] Updating apt and installing git..."
 apt update && apt install -y git
 
 echo "[BoxOS] Fetching install scripts from https://github.com/boxelio/boxel-scripts"
+rm -rf /tmp/boxel-scripts
 git clone https://github.com/boxelio/boxel-scripts /tmp/boxel-scripts
 cd /tmp/boxel-scripts
 chmod +x install/base.sh && install/base.sh
