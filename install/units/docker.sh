cf2c7c37a724de1593cbcac96c0cccea275a0112
Kevin Elliott <kevin@welikeinc.com>
2021-01-20 20:18:26 -0800

    Misc cleanup

--- install/units/docker.sh
+++ install/units/docker.sh
@@ -6,7 +6,7 @@
 # Retrieves and intalls Docker on Ubuntu
 #
 
-apt remove docker docker-engine docker.io containerd runc docker-ce
+apt remove docker docker-engine docker.io containerd runc docker-ce docker-ce-cli containerd.io
 
 echo "[BoxOS] Installing Docker"
 curl -fsSL https://get.docker.com | sh 2>&1 >> /tmp/boxel-docker-install.log
